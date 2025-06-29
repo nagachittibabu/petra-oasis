09f64c49ab45709472bed5221eb78d8777bb7342
nagachittibabu <nagababug997@gmail.com>
2025-06-29 21:20:15 +0530

    created the header and banner

---
+++ src/app/cards/person.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+interface PersonCardProps{
+    imageurl:string
+    name:string
+    specialization:string
+}
+const Person:React.FC<PersonCardProps>= ({imageurl,name,specialization}) => {
+  return (
+    <div >
+        <div>
+        <img src={imageurl} alt="pic" />
+        </div>
+        <h1>{name}</h1>
+        <h2>{specialization}</h2>
+    </div>
+  )
+}
+
+export default Person
