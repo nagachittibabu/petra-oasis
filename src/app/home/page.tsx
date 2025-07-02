7c1abe553205b2ce73463f581463df59475b12d7
savyajajimoggala <savyajajimoggala@gmail.com>
2025-07-02 09:34:36 +0530

    modified files

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,22 +1,25 @@
 import React from 'react'
-import HomeCarousel from '../components/carousel'
-import CoreValueSection from '../components/corevalueSection'
-import ProjectSection from '../components/projectSection'
-import PromiseSection from '../components/promiseSection'
-import ContactPage from '../contact-us/page'
-import TeamSection from '../components/TeamSection'
-import ContactSection from '../components/contactSection'
+import HomeCarousel from '../components/carousel';
+import PromiseSection from '../components/promiseSection';
+import CoreValueSection from '../components/corevalueSection';
+import ProjectSection from '../components/projectSection';
+import TeamSection from '../components/TeamSection';
+import ContactSection from '../components/contactSection';
+import AboutUs from '../about-us/pages';
+
+
 
 const HomePage = () => {
 
   return (
     <div className='w-full h-full  '>
-      <HomeCarousel />
+      <HomeCarousel/>
       <PromiseSection />
       <CoreValueSection />
       <ProjectSection />
       <TeamSection />
       <ContactSection />
+       <AboutUs />
     </div>
   )
 }
